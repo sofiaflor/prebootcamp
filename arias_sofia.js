ec1e73c0c14ab23f2bd9ead20d3549d150e3adaf
sofiaflor <sofiaarias.iccp@gmail.com>
2022-08-12 12:49:08 -0400

    subiendo prediccion

---
+++ arias_sofia.js
@@ -0,0 +1,24 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+}
+//el console.log imprime Naci en 1980//
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//el console.log imprime Naci en 1980//    
+var año= 2006;
+function myBirthYearFunc(año){
+        console.log("Nací en " + año);
+    myBirthYearFunc(año)
+    Nací en 2006
+
+    function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is:10 " + num1);
+            console.log("num2 is: 20" + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+        //el console.log imprime 30//
+        
+        
