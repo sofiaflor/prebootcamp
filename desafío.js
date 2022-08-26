952f6678db92786190221c0e493515e660f8f3c1
sofiaflor <sofiaarias.iccp@gmail.com>
2022-08-26 11:22:21 -0400

    desafio

---
+++ desafío.js
@@ -0,0 +1,11 @@
+var alturaniño=20;
+    function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaniño){
+        if(alturaniño>52){
+         console.log("¡Súbete, chico!");
+        }
+             else{
+            console.log("Lo siento, chico. Tal vez el próximo año");
+        }
+    }
+    
+// console.log("Lo siento, chico. Tal vez el proximo año")//
